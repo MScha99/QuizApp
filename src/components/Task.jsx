5aeaa906f1d9528f04cb90ed0fabe3bafbdf780b
Janek Łuczka <janekluczka@gmail.com>
2024-04-21 18:29:23 +0200

    modified UI

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,108 +3,148 @@
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useEffect } from "react"
 import "./Task.css";
+import { Button, Typography } from '@mui/material';
 
 
 const Task = ({ type, question, choices, onAnswerClick, answerIndex, sentence, setSentence }) => {
 
-
     useEffect(() => {
-        console.log("use effect: ",sentence.join(" "));
+        console.log("use effect: ", sentence.join(" "));
         onAnswerClick(sentence.join(' '))
     }, [sentence]);
 
-
-
-        const handleOnDragEnd = (result) => {
-            if (!result.destination) {
-                return; // Exit the function if dropped outside the droppable area
-              }
-            console.log(result)
-            const items = Array.from(sentence)
-            const [reorderedItem] = items.splice(result.source.index, 1)
-            items.splice(result.destination.index, 0, reorderedItem)
-            setSentence(items)
-           
-                        
-            
+    const handleOnDragEnd = (result) => {
+        if (!result.destination) {
+            return; // Exit the function if dropped outside the droppable area
         }
+        console.log(result)
+        const items = Array.from(sentence)
+        const [reorderedItem] = items.splice(result.source.index, 1)
+        items.splice(result.destination.index, 0, reorderedItem)
+        setSentence(items)
+    }
 
-        const removeWord = (index) => {
-            const newSentence = [...sentence];
-            newSentence.splice(index, 1);
-            setSentence(newSentence);
-            
-        };
-
+    const removeWord = (index) => {
+        const newSentence = [...sentence];
+        newSentence.splice(index, 1);
+        setSentence(newSentence);
+    };
 
     switch (type) {
-      case 'single-choice':
-        return (  <div className='task-container'>
-            <h2 className="question-text">{question}</h2>
-            <ul>{  choices.map((choice, index)=> (
-                <li 
-                key={choice} 
-                onClick={()=> onAnswerClick (choice, index)}
-                className={answerIndex === index ? "selected-answer" : null}
-                >
-                    {choice}
-                </li>
-            ))}</ul>
-            </div>
-          
-        )
-      case 'create-sentence':
-        return (  <div className='task-container'>
-            <h2>{question}</h2>
-
-
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="characters" direction="horizontal">
-                    {(provided) => (
-
-            <div className='draggable-container' style={{ display: 'flex', justifyContent: "center" }} {...provided.droppableProps} ref={provided.innerRef}
-            > 
-                {  sentence.map((word, index)=> (
-                    <Draggable key={word} draggableId={word} index={index}>
-                        {(provided)=> (
-                <div className = "draggable-words" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}
-                onClick={() => removeWord(index)}
-                >
-                    {word}
+        case 'single-choice':
+            return (
+                <div className='task-container'>
+                    <Typography sx={{
+                        fontSize: "24px",
+                        lineHeight: "30px",
+                        color: "#000000",
+                        fontWeight: "bold",
+                        fontFamily: "Inter, sans-serif",
+                        marginBottom: "24px"
+                    }}>
+                        Wybierz poprawną odpowiedź
+                    </Typography>
+                    <Typography sx={{
+                        fontSize: "24px",
+                        lineHeight: "30px",
+                        color: "#000000",
+                        fontWeight: "bold",
+                        fontFamily: "Inter, sans-serif",
+                        marginBottom: "24px",
+                        maxWidth: "100%",
+                        textAlign: "center"
+                    }}>
+                        {question}
+                    </Typography>
+                    {/* <ul>
+                        {choices.map((choice, index) => (
+                            <li
+                                key={choice}
+                                onClick={() => onAnswerClick(choice, index)}
+                                className={answerIndex === index ? "selected-answer" : null}
+                            >
+                                {choice}
+                            </li>
+                        ))}
+                    </ul> */}
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        {choices.map((choice, index) => (
+                            <Button
+                                key={choice}
+                                onClick={() => onAnswerClick(choice, index)}
+                                variant={answerIndex === index ? "contained" : "outlined"}
+                                color="primary"
+                                sx={{ marginBottom: "16px", width: "480px" }}
+                            >
+                                {choice}
+                            </Button>
+                        ))}
+                    </div>
                 </div>
-                )}
-                </Draggable>
-            ))}
-            {provided.placeholder}
+            )
+        case 'create-sentence':
+            return (<div className='task-container'>
+                <Typography sx={{
+                    fontSize: "24px",
+                    lineHeight: "30px",
+                    color: "#000000",
+                    fontWeight: "bold",
+                    fontFamily: "Inter, sans-serif",
+                    marginBottom: "24px"
+                }}>
+                    Wybierz poprawną odpowiedź
+                </Typography>
+                <Typography sx={{
+                    fontSize: "24px",
+                    lineHeight: "30px",
+                    color: "#000000",
+                    fontWeight: "bold",
+                    fontFamily: "Inter, sans-serif",
+                    marginBottom: "24px",
+                    maxWidth: "100%",
+                    textAlign: "center"
+                }}>
+                    {question}
+                </Typography>
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <Droppable droppableId="characters" direction="horizontal">
+                        {(provided) => (
+                            <div className='draggable-container' style={{ display: 'flex', justifyContent: "center" }} {...provided.droppableProps} ref={provided.innerRef}>
+                                {sentence.map((word, index) => (
+                                    <Draggable key={word} draggableId={word} index={index}>
+                                        {(provided) => (
+                                            <div className="draggable-words" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}
+                                                onClick={() => removeWord(index)}
+                                            >
+                                                {word}
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </div>
+
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+                <div className='choices-container'>{choices.map((choice, index) => (
+                    <button
+                        className='choice-button'
+                        key={choice}
+                        onClick={() => {
+                            setSentence(prevSentence => [...prevSentence, choice]);
+                        }}
+                        disabled={sentence.includes(choice)}
+                    >
+                        {choice}
+                    </button>
+                ))}</div>
             </div>
-
-            )}
-            </Droppable>
-            </DragDropContext>
-
-
-
-            <div className='choices-container'>{  choices.map((choice, index)=> (
-                <button 
-                className = 'choice-button'
-                key={choice} 
-                onClick={() => {
-                    setSentence(prevSentence => [...prevSentence, choice]);
-                    
-                }}
-
-                disabled={sentence.includes(choice)}
-                
-                >
-                    {choice}
-                </button>
-            ))}</div>
-            </div>
-          
-        )
-      default:
-        return null;
+            )
+        default:
+            return null;
     }
-  };
-  
-  export default Task;
+};
+
+export default Task;
--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './Tutorial.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookOpen} from '@fortawesome/free-solid-svg-icons';
+import { Button } from '@mui/material';
 
 export default function Tutorial() {
     const [tutorial, setTutorial] = useState(false);
@@ -12,33 +11,33 @@ export default function Tutorial() {
 
     return (
         <>
-        <button
-        onClick={toggleTutorial} className='btn-tutorial'><FontAwesomeIcon icon={faBookOpen} /></button>
+            <Button variant='contained' onClick={toggleTutorial}>Przewodnik</Button>
 
-         {tutorial && (
-            <div className="tutorial">
-            <div onClick={toggleTutorial} className="overlay">
-                <div className="tutorial-content">
-                    <h2 className='tutorialContent'>Samouczek</h2>
-                    <p className='tutorialContent'>Terve! Kuka sinä olet? - Cześć! Kim jesteś?<br/>
-                                                    Minä olen Elsa. - Jestem Elsa.<br/>
-                                                    Anteeksi, kuka sinä olet? - Przepraszam, kim jesteś?<br/>
-                                                    Minä olen Otso. - Jestem Otso.<br/>
-                                                    Tervetuloa! - Witam!<br/>
-                                                    Jee! Onnea! - Jej! Powodzenia!<br/>
-                                                    Liisa, sinä olet hauska. - Liisa, jesteś zabawna.<br/>
-                                                    Kippis ja paljon onnea! - Na zdrowie i gratulacje!<br/>
-                                                    Elsa on nainen. - Elsa jest kobietą.<br/>
-                                                    Väinö on mies. - Väinö jest mężczyzną.<br/>
-                                                    Anteeksi, kuka hän on? - Przepraszam, kim ona jest?<br/>
-                                                    Hän on lapsi. - Ona jest dzieckiem.<br/>
-                                                    Sinä olet todella komea. - Jesteś naprawde przystojny.<br/></p>
-                    <button
-                    onClick={toggleTutorial} className='close-tutorial'>Zamknij</button>
+            {tutorial && (
+                <div className="tutorial">
+                    <div onClick={toggleTutorial} className="overlay">
+                        <div className="tutorial-content">
+                            <h2 className='tutorialContent'>Samouczek</h2>
+                            <p className='tutorialContent'>
+                                Terve! Kuka sinä olet? - Cześć! Kim jesteś?<br />
+                                Minä olen Elsa. - Jestem Elsa.<br />
+                                Anteeksi, kuka sinä olet? - Przepraszam, kim jesteś?<br />
+                                Minä olen Otso. - Jestem Otso.<br />
+                                Tervetuloa! - Witam!<br />
+                                Jee! Onnea! - Jej! Powodzenia!<br />
+                                Liisa, sinä olet hauska. - Liisa, jesteś zabawna.<br />
+                                Kippis ja paljon onnea! - Na zdrowie i gratulacje!<br />
+                                Elsa on nainen. - Elsa jest kobietą.<br />
+                                Väinö on mies. - Väinö jest mężczyzną.<br />
+                                Anteeksi, kuka hän on? - Przepraszam, kim ona jest?<br />
+                                Hän on lapsi. - Ona jest dzieckiem.<br />
+                                Sinä olet todella komea. - Jesteś naprawde przystojny.<br />
+                            </p>
+                            <button onClick={toggleTutorial} className='close-tutorial'>Zamknij</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-         )}
+            )}
         </>
     );
 }
